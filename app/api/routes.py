ba840bd90c4ff3a47eebfb25fa3f45c7bd27cd12
Alejandro Díaz Román <alejandrodiazroman.contacto@gmail.com>
2022-07-24 00:36:57 +0200

    Api Blueprint & some endpoints (/users/<id> & /users)

---
+++ app/api/routes.py
@@ -0,0 +1,7 @@
+from this import d
+from app.api import Blueprint
+
+
+
+def bad_request():
+    pass
--- app/models.py
+++ app/models.py
@@ -1,14 +1,40 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
-from flask import current_app
+from flask import current_app, url_for
 from flask_login import UserMixin
+from flask_sqlalchemy import Pagination
+from sqlalchemy import false
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 
 
-class User(UserMixin, db.Model):
+class PaginatedAPIMixin(object):
+
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page, **kwargs) if resources.has_prev else None
+            }
+        }
+
+        return data
+
+
+
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(128), index = True, unique = True)
@@ -58,6 +84,35 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
+
+    def to_dict(self, include_email=False, avatar_size=128):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'rutine_count': self.routines.count(),
+            'session_count': self.sessions.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'routines': url_for('api.get_routines', id=self.id),
+                'sessions': url_for('api.get_sessions', id=self.id),
+                'avatar': self.avatar(avatar_size)
+            }   
+        }
+
+        if include_email:
+            data['email'] = self.email
+
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email']:
+            if field in data:
+                setattr(self, field, data[field])
+        
+        if new_user and 'passwird' in data:
+            self.set_password(data['password'])
+
 
 
 
